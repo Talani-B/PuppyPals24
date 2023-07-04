422492b17d79f353f252b78add44385af053d732
Talani Balogun <Taloni5992@gmail.com>
2023-07-03 20:46:00 -0400

    changes to files

--- Block 24/src/App.jsx
+++ Block 24/src/App.jsx
@@ -1,32 +1,24 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
-import './App.css'
+console.log(puppies)
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-     <div>
-     { 
-   puppies.map((puppy) => {
-     return <p>{puppy.name}</p>
-   })
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
-     </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
