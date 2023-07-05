6f4a744038dde18d5c460afc56a291d3a4e32d91
Talani Balogun <Taloni5992@gmail.com>
2023-07-05 10:29:43 -0400

    changes to files

--- Block 24/src/App.jsx
+++ Block 24/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
+console.log(puppies)
 
 function App() {
 
@@ -17,7 +18,5 @@ function App() {
     </div>
   );
 }
-     
-    
 
 export default App
