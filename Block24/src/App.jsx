bdace0e03b5e2ba8abcdbaae79718abbb8b0bc72
Talani Balogun <Taloni5992@gmail.com>
2023-07-12 09:03:43 -0400

    changes made to all files

--- Block24/src/App.jsx
+++ Block24/src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
+import './index.css';
 
 function App() {
- 
-
-  return (
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log(puppyList)
   
-      <div>
-       
-      </div>
+  function handleClick() {
+     //some logic here
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
+  return (
+  <div className='App'> 
+  { featuredPup && (
+   < div>
+   <h2>{featuredPup.name}</h2>
+  <ul>
+    <li>Age: {featuredPup.age}</li>
+    <li>Email: {featuredPup.email}</li>
+  </ul>
+  </div>
+  )}
+  {featPupId !==null&& <p>{featPupId}</p>}
+  {puppies.map((puppy) => {
+     return (
+  
+      <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+    
+      );
+   })} 
+   </div>
       
       
    
-  )
+  );
 }
 
 export default App
